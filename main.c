e75e079545926b24e7c9f9294894475883285a16
cosc301 <cosc301@csvm.(none)>
2013-10-04 21:30:12 -0400

    started trying to tokenify input; edited comments

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 	October, 2013
 	Contributers:
 		Carrie Burgess
-		Mac Lanphier
+		Zombie bastard
 		Brett Rojec
 */
 
@@ -20,6 +20,9 @@
 #include <poll.h>
 #include <signal.h>
 
+int parallel=0; /*B: I don't think mode() is needed, just use this global int (bool)
+	          1: process things in parallel (actually, any nonzero)
+	          0: process sequentially */
 void mode() {
 	//this will change the mode to either parallel or sequential
 	//sequential: use waitpd() call
@@ -27,13 +30,15 @@ void mode() {
 }
 
 void exit() {
-	//this exits everything
-	exit;
+	//exit normally (also, this may not need its own function)
+	exit(0);
 }
 
-char** deligate(const char** arr) {
-	//we can definitely rename this...
+char** handle_commands(const char** arr) {
+	//C: we can definitely rename this...
+	//B: I did, I called it handle_commands() in main().
 	
+	/*C:
 	//this should take in an array of pointers from tokenify
 	//first, comb through to find comments.  Change comment to null character
 	//next, go through tokens, break into commands based on semicolons 
@@ -42,28 +47,42 @@ char** deligate(const char** arr) {
 		//make sure to check character after 'mode' to see if either a 's', 'p'
 		//or says 'parallel' or 'sequential'
 	//for rest, check if command/process, or if an option (ex -c)
-	//use execv (?) to make process work
+	use execv (?) to make process work*/
 	
 	return NULL;
+}
+
+int num_toks(const char* str){
+	char* str2 = strdup(str); //strtok() fucks w/ input, so copy str
+	int toks = 0;
+	if(strtok(str2, ";")!=NULL){
+		toks++;
+		while(strtok(NULL, ";")!=NULL)
+			toks++;
+	}
+	free(str2);
+	return toks;
 }
 
 char** tokenify(const char* str){
 	//break str up into appropriate tokens,
 	//and return an array of the results
 	
-	//string should include no tabs/ spaces
+	//string should include no tabs/ spaces //B: why?
 	
 	//might want to reuse code...
+	printf("number of tokens in %s: %d\n", str, num_toks(str));
+	
 	return NULL;
 }
 
 int main(int argc, char **argv) {
 	char* input = (char*) malloc(sizeof(char)*255);
 	printf(">>>");
-	//might want another while loop to recursively keep asking user what wants
-	//until get EOF/ ctrl+D signal
+	//C: might want another while loop to recursively keep asking user what wants
+	//until get EOF/ ctrl+D signal <--that's what this does ~Brett
 	while(fgets(input, 255, stdin)!=NULL){
-		handle_token(tokenify(input));
+		handle_commands(tokenify(input));
 		printf(">>>");
 	}
 	printf("\n");
